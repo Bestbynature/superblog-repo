5e94d5f4e3aef53672520a51361674c333f9c34d
Dammy <bestbynature2010@gmail.com>
2024-03-19 07:07:49 +0100

    Add arrays for mapping

--- src/contact.js
+++ src/contact.js
@@ -33,13 +33,17 @@ const Contact = () => {
       }
       setCount(count + 1)
     }
+
+    const names = ["Sina", "Ali", "Reza", "Mina", "Sara"];
+
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     
     const handleDecrease = () => {
       if(count !== 0) {
         setCount(count - 1)
       }
     }
-  }
+  
 
   return (
     <div className="wrapper">
