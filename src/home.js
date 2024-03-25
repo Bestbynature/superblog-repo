0df0b118c8d7056020ee4c43a447e8359d041a9d
Dammy <bestbynature2010@gmail.com>
2024-03-25 20:14:51 +0100

    Use the map method

--- src/home.js
+++ src/home.js
@@ -16,12 +16,28 @@ const Home = () => {
       setBlogs(data)
     })
   }, []) 
+
+  const buttonData = [
+    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "x", "-", "+"
+  ]
+
  
   return (
     <>
       {blogs && <BlogList blogs={blogs} title="All The Blogs" />}
       {blogs && <BlogList blogs={blogs.filter((blog) => blog.title === "Covid19")} title="Covid19" />}
       {value}
+      {buttonData.map(value => <button>{value}</button>)}
+      {/* {buttonData.map((value)=>(
+        <button key={value}>{value}</button>
+        
+      ))} */}
+      <ul>
+        <li>item one</li>
+        <li>item two</li>
+        <li>item three</li>
+        
+      </ul>
     </>
   );
 };
