fdbd52455cf8e5dd06fddcf6522ef4d5909c1776
Dammy <bestbynature2010@gmail.com>
2024-03-26 18:59:04 +0100

    remove unnecessary codes

--- src/home.js
+++ src/home.js
@@ -4,7 +4,6 @@ import BlogList from "./bloglist";
 const Home = () => {
 
   const [blogs, setBlogs] = useState(null);
-  const [value, setValue] = useState(23)
 
   useEffect(()=>{
     console.log("use Effect is running now")
@@ -17,21 +16,11 @@ const Home = () => {
     })
   }, []) 
 
-  const buttonData = [
-    "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "x", "-", "+"
-  ]
-
  
   return (
     <>
       {blogs && <BlogList blogs={blogs} title="All The Blogs" />}
       {blogs && <BlogList blogs={blogs.filter((blog) => blog.title === "Covid19")} title="Covid19" />}
-      {value}
-      {buttonData.map(value => <button>{value}</button>)}
-      {/* {buttonData.map((value)=>(
-        <button key={value}>{value}</button>
-        
-      ))} */}
       <ul>
         <li>item one</li>
         <li>item two</li>
